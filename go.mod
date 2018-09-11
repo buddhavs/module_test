5897b828c40da4e985d863e5aacf454e80b04e8e
shuohuan <shuohuan@netapp.com>
2018-09-11 15:22:47 -0700

    v2.0.0

--- go.mod
+++ go.mod
@@ -1,1 +1,1 @@
-module github.com/buddhavs/module_test
+module github.com/buddhavs/module_test/v2
--- pkg.go
+++ pkg.go
@@ -4,5 +4,5 @@ import "fmt"
 
 //Run as running...
 func Run() {
-	fmt.Println("Running v1.0.3!")
+	fmt.Println("Running v2.0.0!")
 }
